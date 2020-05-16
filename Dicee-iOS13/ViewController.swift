ca08f678570a33b9dd949585e6d35ba49cfdb839
Ilona Sellenberkova <ilona.sell@gmail.com>
2020-05-16 23:16:33 +0200

    IBOutlet, IBAction, Image Literals, random nums

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -11,13 +11,15 @@ import UIKit
 class ViewController: UIViewController {
     
     // IBOutlets allows me to reference to UI element
-    @IBOutlet weak var diceImageView1: UIImageView!
-    @IBOutlet weak var diceImageView2: UIImageView!
+  @IBOutlet weak var diceImageView1: UIImageView!
+  @IBOutlet weak var diceImageView2: UIImageView!
   
-    @IBAction func rollButtonPressed(_ sender: UIButton) {
-      //      new random numbers generate when click on btn - inside of fce
-        diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ][Int.random(in: (0..<6))]
-        diceImageView2.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ][Int.random(in: (0..<6))]
-    }
+  let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ] // #imageLiteral(resourceName: "DiceOne")
+  
+  @IBAction func rollButtonPressed(_ sender: UIButton) {
+    //  new random element is picked from diceArray and assign as new image into UI dice
+    diceImageView1.image = diceArray.randomElement() // [Int.random(in: (0..<6))]
+    diceImageView2.image = diceArray.randomElement()
+  }
 }
 
